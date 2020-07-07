64698377c48394f2234548e8f6e95908e102cfd5
lewy95 <171133755@qq.com>
2020-07-07 18:20:15 +0800

    debug consumer boot failure

--- circle-achieve-consumer/src/main/java/cn/xzxy/lewy/circleachieve/CircleAchieveConsumerApplication.java
+++ circle-achieve-consumer/src/main/java/cn/xzxy/lewy/circleachieve/CircleAchieveConsumerApplication.java
@@ -3,6 +3,7 @@ package cn.xzxy.lewy.circleachieve;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.unit.DataSize;
 
@@ -12,6 +13,7 @@ import javax.servlet.MultipartConfigElement;
  * @author lewy95
  **/
 @SpringBootApplication
+@EnableFeignClients("cn.xzxy.lewy.circleachieve")
 public class CircleAchieveConsumerApplication {
 
     public static void main(String[] args) {
